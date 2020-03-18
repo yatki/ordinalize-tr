621d392cae8e74bba5585be15f14a9231d418ac4
yatki <mehmet@yatki.com>
2020-03-19 01:27:40 +0300

    Rename umd name

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,33 +1,33 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import typescript from "rollup-plugin-typescript2";
-import { terser } from "rollup-plugin-terser";
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const pkg = require("./package.json");
+const pkg = require('./package.json');
 
 const config = [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
         file: pkg.main,
-        format: "cjs",
-        sourcemap: true
+        format: 'cjs',
+        sourcemap: true,
       },
       {
         file: pkg.module,
-        format: "esm",
-        sourcemap: true
+        format: 'esm',
+        sourcemap: true,
       },
       {
         file: pkg.browser,
-        format: "umd",
+        format: 'umd',
         sourcemap: true,
-        name: "ordinalTR"
-      }
+        name: 'ordinalizeTR',
+      },
     ],
-    plugins: [typescript(), terser({ output: { comments: false } })]
-  }
+    plugins: [typescript(), terser({ output: { comments: false } })],
+  },
 ];
 
 export default config;
